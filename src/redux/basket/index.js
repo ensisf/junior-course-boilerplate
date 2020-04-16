5ad9ff5222907ff95d2ca8870d32e84a02f8b81c
Bohdan Zamchenco <zamchenco@gmail.com>
2020-04-16 12:27:33 +0300

    Add basket store

---
+++ src/redux/basket/index.js
@@ -0,0 +1,8 @@
+import reducer from "./reducers.js";
+
+export * from "./actions.js";
+export * from "./types.js";
+export * from "./operations.js";
+export * from "./selectors";
+
+export default reducer;
