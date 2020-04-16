6d67b04fa847a84efaa7694031b7ffa1f1fef5a1
Bohdan Zamchenco <zamchenco@gmail.com>
2020-04-16 12:30:48 +0300

    Move app from containers

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "rdx";
 
-import styled from "./index.module.scss";
+import styled from "./App.module.scss";
 
 import { Home } from "pages/home";
 import { Product } from "pages/product";
-import { NotFound } from "pages/not-found";
+import { Page404 } from "pages/page-404";
 
 const App = () => {
   return (
@@ -16,7 +16,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/p/:productId" component={Product} />
-          <Route path="*" component={NotFound} />
+          <Route path="*" component={Page404} />
         </Switch>
       </ConnectedRouter>
     </div>
